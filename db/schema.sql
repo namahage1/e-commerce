693b9fc6e12510c8c10fbfa1d709275f852dcd5c
Kasumi Akiyama★ <shellyakiyama@gmail.com>
2024-05-20 23:09:03 -0600

    changed file names

--- db/schema.sql
+++ db/schema.sql
@@ -12,13 +12,16 @@ create table department (
     id serial primary key,
     title varchar(30) unique not null,
     salary decimal not null,
-    department_id integer not null references department(id)
+    department_id integer not null,
+    foreign key (department_id) references department(id)
  );
 
 create table employee(
     id serial primary key,
     first_name varchar(30) not null,
     last_name varchar(30) not null,
-    role_id integer not null references role(id),
-    manager_id integer references employee(id) 
+    role_id integer not null,
+    manager_id integer,
+    foreign key (role_id)references role(id),
+    foreign key (manager_id) references employee(id)
 );
--- server.js
+++ server.js
@@ -20,26 +20,24 @@ const initialize = async () => {
         choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role']
     }]).then(answers => {
         console.log(`user choice ${answers.user_choice}`);
-
-
         if (answers.user_choice === "view all departments") {
             db.query("SELECT * FROM department", function (err, data) {
                 console.log(err)
                 console.log(data.rows)
             })
         }else if(answers.user_choice === "view all roles") {
-            db.query("SELECT * FROM department", function (err, data) {
+            db.query("SELECT * FROM role", function (err, data) {
                 console.log(err)
                 console.log(data.rows)
             })
         }else if(answers.user_choice === "view all employees") {
-            db.query("SELECT * FROM department", function (err, data) {
+            db.query("SELECT * FROM employee", function (err, data) {
                 console.log(err)
                 console.log(data.rows)
             })
         }else if(answers.user_choice === "add a department") {
             //insert
-            db.query("SELECT * FROM department", function (err, data) {
+            db.query("INSERT INTO department(name) VALUES ()", function (err, data) {
                 console.log(err)
                 console.log(data.rows)
             })
@@ -58,11 +56,46 @@ const initialize = async () => {
             
         }else if(answers.user_choice === "update an employee role") {
             //update 
-            db.query("SELECT * FROM department", function (err, data) {
-                console.log(err)
-                console.log(data.rows)
+            //prompted to select an employee to update and their new role and this information is updated in the database
+            db.query("SELECT id, first_name,last_name FROM employee", function (err, data) {
+                
+                let employeeList = data.rows;
+             //   console.log(employeeList);
+            
+                inquirer.prompt([{
+                    type: 'list',
+                    name: 'user_choice',
+                    message: 'select an employee',
+                    choices: employeeList
+                }]).then(answers => {
+                    console.log(`user choice ${answers.user_choice}`);
+                }); console.log(err)
+               
             })
         }
     });
 }
+
+// function deleteData(db,tableName,column){
+//     const sql = `DELETE FROM movies WHERE id = $1`;
+//     const params = [req.params.id];
+  
+//     pool.query(sql, params, (err, result) => {
+//       if (err) {
+//         res.statusMessage(400).json({ error: err.message });
+//       } else if (!result.rowCount) {
+//         res.json({
+//           message: 'data not found'
+//         });
+//       } else {
+//         res.json({
+//           message: 'deleted',
+//           changes: result.rowCount,
+//           id: req.params.id
+//         });
+//       }
+//     });
+
+  
+// }
 initialize();
