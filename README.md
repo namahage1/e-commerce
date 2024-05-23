9d36b8aa434a006db965d1ed40edd3e818eb50dc
Kasumi Akiyama★ <shellyakiyama@gmail.com>
2024-05-22 19:00:15 -0600

    added update and add functionalities

---
+++ README.md
@@ -0,0 +1,35 @@
+# Employee Tracker
+
+## Description
+
+- Database has table names, employee, role and department
+- This application asks a user to chose from list and based on the user choice
+- What problem does it solve?
+- What did you learn?
+
+## Table of Contents (Optional)
+
+- [Installation](#installation)
+- [Usage](#usage)
+
+
+## Installation
+- npm i on terminal 
+- psql -U postgres with password test
+- go to 
+
+
+## Usage
+
+Provide instructions and examples for use. Include screenshots as needed.
+
+To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
+
+    ```md
+    ![alt text](assets/images/screenshot.png)
+    ```
+
+---
+
+🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
+
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const inquirer = require("inquirer");
-
+require("console.table");
 const { Pool } = require("pg");
 
 const pool = new Pool({
@@ -23,54 +23,109 @@ const initialize = async () => {
         if (answers.user_choice === "view all departments") {
             db.query("SELECT * FROM department", function (err, data) {
                 console.log(err)
-                console.log(data.rows)
+                console.table(data.rows)
+                initialize();
             })
         }else if(answers.user_choice === "view all roles") {
             db.query("SELECT * FROM role", function (err, data) {
                 console.log(err)
-                console.log(data.rows)
+                console.table(data.rows)
+                initialize();
             })
         }else if(answers.user_choice === "view all employees") {
             db.query("SELECT * FROM employee", function (err, data) {
                 console.log(err)
-                console.log(data.rows)
+                console.table(data.rows)
+                initialize();
             })
         }else if(answers.user_choice === "add a department") {
             //insert
-            db.query("INSERT INTO department(name) VALUES ()", function (err, data) {
-                console.log(err)
-                console.log(data.rows)
+            inquirer.prompt({
+                type:"input",
+                name:"department_name",
+                message: "Enter the name of the department"
             })
+            .then(answers =>{
+                db.query("INSERT INTO department(name) VALUES ($1)",[answers.department_name], function (err, data) {
+                    console.log("New department is added!");
+                   
+                    initialize();
+                })
+            })
+ 
         }else if(answers.user_choice === "add a role") {
-            //insert
-            db.query("SELECT * FROM department", function (err, data) {
-                console.log(err)
-                console.log(data.rows)
+            db.query("SELECT id AS value, name FROM department", function (err, data) {
+                
+                let departmentList = data.rows;
+            //TODO take user input and add to the query
+            inquirer.prompt([
+                {
+                    type:"input",
+                    name:"title",
+                    message: "Enter the title of the role"
+                },
+                {
+                    type:"input",
+                    name:"salary",
+                    message: "Enter the salary of the role"
+                },
+                {
+                    type:"list",
+                    name:"department_id",
+                    message: "Select department",
+                    choices: departmentList
+                }
+            ])
+            .then(answers =>{
+                db.query("INSERT INTO role(title, salary, department_id) VALUES ($1, $2, $3)",[answers.title, answers.salary, answers.department_id], function (err, data) {
+                    console.log("New role is added!");
+                   
+                    initialize();
+                })
             })
+        })
         }else if(answers.user_choice === "add an employee") {
             //insert
-                db.query("SELECT * FROM department", function (err, data) {
+            inquirer.prompt([{
+                type:'input',
+                name: 'employee',
+                message: 'Input first name, last name, role id and manager id (empty for no manager)'
+            }]);//TODO take user input and add to query
+                db.query(`INSERT INTO employee VALUES`, function (err, data) {
                     console.log(err)
-                    console.log(data.rows)
+                    console.table(data.rows)
+                    initialize();
                 })
             
         }else if(answers.user_choice === "update an employee role") {
             //update 
             //prompted to select an employee to update and their new role and this information is updated in the database
-            db.query("SELECT id, first_name,last_name FROM employee", function (err, data) {
+            db.query("SELECT id AS value, CONCAT(first_name, ' ', last_name) AS name FROM employee", function (err, data) {
                 
                 let employeeList = data.rows;
-             //   console.log(employeeList);
+              
+            db.query("SELECT id AS value, title AS name FROM role", function (err, data) {
+                
+                    let roleList = data.rows;
             
                 inquirer.prompt([{
                     type: 'list',
                     name: 'user_choice',
                     message: 'select an employee',
                     choices: employeeList
+                },
+                {
+                    type: 'list',
+                    name: 'role_choice',
+                    message: 'What is the new role?',
+                    choices: roleList
                 }]).then(answers => {
-                    console.log(`user choice ${answers.user_choice}`);
-                }); console.log(err)
-               
+                    db.query("UPDATE employee SET role_id = $1 WHERE id = $2",[answers.role_choice, answers.user_choice],function(err,data){
+                        console.log("Update successful!");
+                        initialize();
+                    });
+                });
+            }) 
             })
         }
     });
