8ef59b343c897e7663c83fa667ad2b2c61909052
Kasumi Akiyama★ <shellyakiyama@gmail.com>
2024-05-29 07:35:02 +0900

    removed develop folder, updated models and added routers

--- db/schema.sql
+++ db/schema.sql
@@ -1,27 +1,6 @@
-DROP database if exists employee_info;
-create database employee_info;
+-- DROP DATABASE
+DROP DATABASE IF EXISTS ecommerce_db;
 
-\c employee_info;
+-- CREATE DATABASE
+CREATE DATABASE ecommerce_db;
 
-create table department (
-  id serial primary key,
-  name VARCHAR(30) NOT NULL unique
-);
--- role
- create table role(
-    id serial primary key,
-    title varchar(30) unique not null,
-    salary decimal not null,
-    department_id integer not null,
-    foreign key (department_id) references department(id)
- );
-
-create table employee(
-    id serial primary key,
-    first_name varchar(30) not null,
-    last_name varchar(30) not null,
-    role_id integer not null,
-    manager_id integer,
-    foreign key (role_id)references role(id),
-    foreign key (manager_id) references employee(id)
-);
