a4da1356995639271b77baf0a1538219d97c6c1e
Kasumi Akiyama★ <shellyakiyama@gmail.com>
2024-05-29 08:36:48 +0900

    added delete and update routers in category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -31,12 +31,41 @@ router.post('/', async (req, res) => {
   res.json(result);
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  const categoryId = req.params.id;
+  const updatedData = req.body;
+  try {
+    const category = await Category.findByPk(categoryId);
+  if(!category){
+    return res.status(404).json({message:'Category not found'});
+  }
+
+  await category.update(updatedData);
+  res.json(category);
+  }catch(error){
+    console.error(error);
+    res.status(500).json({message:'Internal Service Error'});
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try{
+    const categoryData = await Category.destroy({
+      where:{
+        id:req.params.id
+      }
+    });
+    if(!categoryData){
+      res.status(404).json({message:'Category not found'});
+      return;
+    }
+    res.status(200).json(categoryData);
+  }catch(error){
+    console.error(error);
+    res.status(500).json({message:'Internal Service Error'});
+  }
 });
 
 module.exports = router;
